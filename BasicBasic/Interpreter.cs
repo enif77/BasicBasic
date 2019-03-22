e60d0c1f070bf9df68782994dbdcd59bfe59699d
Premysl Fara <enif@email.cz>
2019-03-22 16:40:08 +0100

    Initial commit. Interpreter base.

---
+++ BasicBasic/Interpreter.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicBasic
+{
+    public class Interpreter
+    {
+        #region constants
+
+        public readonly int MaxLabel = 99;
+
+        #endregion
+
+
+        #region ctor
+
+        public Interpreter()
+        {
+            // label :: integer <1 .. 99>
+            _programLines = new ProgramLine[MaxLabel + 1];
+
+            // variable = float <'A' .. 'Z'>
+            _vars = new float['Z' - 'A'];
+        }
+
+        #endregion
+
+
+        #region public
+
+        public class InterpreterException : Exception
+        {
+            public InterpreterException() : base()
+            {
+            }
+
+            public InterpreterException(string message) : base(message)
+            {
+            }
+
+            public InterpreterException(string message, Exception innerException) : base(message, innerException)
+            {
+            }
+        }
+
+
+        public int Interpret(string source)
+        {
+            if (source == null) throw new InterpreterException("A source expected.");
+
+            ScanForLabels(source);
+            InterpretImpl(source);
+            
+            return 0;
+        }
+
+
+        #endregion
+
+
+        #region private
+        
+        private void InterpretImpl(string source)
+        {
+            var programLine = NextProgramLine(0);
+            while (programLine != null)
+            {
+                programLine = InterpretLine(source, programLine);
+            }
+        }
+
+
+        private ProgramLine InterpretLine(string source, ProgramLine programLine)
+        {
+            Console.WriteLine("{0:000} -> {1}", programLine.Label, source.Substring(programLine.Start, (programLine.End - programLine.Start) + 1));
+
+            return NextProgramLine(programLine.Label);
+        }
+
+
+        private ProgramLine NextProgramLine(int fromLabel)
+        {
+            // Skip program lines without code.
+            for (var label = fromLabel; label < _programLines.Length; label++)
+            {
+                if (_programLines[label] != null)
+                {
+                    return _programLines[label];
+                }
+            }
+
+            return null;
+        }
+
+
+        private void ScanForLabels(string source)
+        {
+            ProgramLine programLine = null;
+            var atLineStart = true;
+            var line = 1;
+            var i = 0;
+            for (; i < source.Length; i++)
+            {
+                var c = source[i];
+
+                if (atLineStart)
+                {
+                    programLine = new ProgramLine();
+
+                    // Skip white chars.
+                    if (c <= ' ' && c != '\n')
+                    {
+                        continue;
+                    }
+
+                    // Label.
+                    if (char.IsDigit(c))
+                    {
+                        programLine.Start = i;
+                        var label = 0;
+                        while (char.IsDigit(c))
+                        {
+                            label = label * 10 + (c - '0');
+
+                            i++;
+                            if (i >= source.Length)
+                            {
+                                break;
+                            }
+
+                            c = source[i];
+                        }
+
+                        if (label < 1 || label > MaxLabel)
+                        {
+                            throw new InterpreterException(string.Format("Label {0} at line {1} out of <1 ... {2}> rangle.", label, line, MaxLabel));
+                        }
+
+                        if (_programLines[label - 1] != null)
+                        {
+                            throw new InterpreterException(string.Format("Label {0} redefinition at line {1}.", label, line));
+                        }
+
+                        // Remember this line's label.
+                        programLine.Label = label;
+
+                        // Remember this line.
+                        _programLines[label - 1] = programLine;
+
+                        // Re read the char behind the label.
+                        i--;
+
+                        atLineStart = false;
+                    }
+                }
+
+                if (c == '\n')
+                {
+                    // The character before '\n'.
+                    programLine.End = i - 1;
+
+                    // We are done with this line.
+                    programLine = null;
+
+                    // Starting the next line.
+                    line++;
+                    atLineStart = true;
+                }
+            }
+
+            // The last line does not ended with the '\n' character.
+            if (programLine != null)
+            {
+                programLine.End = source.Length - 1;
+                if (programLine.End < 0)
+                {
+                    programLine.End = 0;
+                }
+            }
+        }
+
+
+        private class ProgramLine
+        {
+            public int Label { get; set; }
+            public int Start { get; set; }
+            public int End { get; set; }
+
+
+            public override string ToString()
+            {
+                return string.Format("{0}: {1} - {2}", Label, Start, End);
+            }
+        }
+
+
+        private ProgramLine[] _programLines;
+        private float[] _vars;
+
+        #endregion
+    }
+}
---
+++ BasicBasicApp/Program.cs
@@ -0,0 +1,64 @@
+﻿/* BasicBasic - (C) 2019 Premysl Fara 
+ 
+BasicBasic is available under the zlib license:
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+ 
+ */
+
+namespace BasicBasicApp
+{
+    using BasicBasic;
+    using System;
+
+
+    static class Program
+    {
+        static int Main(string[] args)
+        {
+            //if (args.Length < 1)
+            //{
+            //    Console.WriteLine("Usage: app.exe [ ... ] input.bas");
+
+            //    return 1;
+            //}
+
+            try
+            {
+                var interpreter = new Interpreter();
+                interpreter.Interpret(_source);
+
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+
+                return 1;
+            }
+
+            return 0;
+        }
+
+        static string _source = @"1
+ 5 FOR I = 1 TO I = 10 DO
+10 PRINT ""Hello""
+15
+20 NEXT I
+
+30";
+    }
+}
